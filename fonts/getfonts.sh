ff92a70f0a7927d0bf8962f2b3aa95b66d2dd47a
David Fölk <david@foelk.com>
2024-10-17 11:28:58 +0200

    fetch -O -> fetch -o

--- fonts/getfonts.sh
+++ fonts/getfonts.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-fetch -O /tmp/CodeNewRoman.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CodeNewRoman.zip &&
+fetch -o /tmp/CodeNewRoman.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CodeNewRoman.zip &&
 unzip -d /tmp/CodeNewRoman /tmp/CodeNewRoman.zip &&
 mv /tmp/CodeNewRoman/CodeNewRomanNerdFont-Regular.otf $HOME/.config/fonts/ &&
 rm -rf /tmp/CodeNewRoman /tmp/CodeNewRoman.zip
